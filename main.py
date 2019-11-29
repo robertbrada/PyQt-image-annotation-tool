d18977be47ea6ad6102087f0720cc443f38289b6
RobertBrada <bert.blader@gmail.com>
2019-11-29 21:54:06 +0100

    getting filenames from folder

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import os
 import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+# ======================================================================
+
+# folder with images we want to label (don't forget "/" at the end)
+input_folder = './data/images1/'
+
+# labels we want to use
+labels = ["label1", "label2", "label3"]
+
+# output csv file
+output_file = 'output.csv'
+
+# allowed file extensions
+file_extensions = ('.jpg', '.png', '.jpeg')
+
+
+# ======================================================================
+def get_img_paths(dir, extensions=''):
+    '''
+    :param dir: folder with files
+    :param extensions: tuple with file endings. e.g. ('.jpg', '.png')
+    :return: list of all filenames
+    '''
+
+    filenames = []
+
+    for filename in os.listdir(dir):
+        if filename.lower().endswith(extensions):
+            filenames.append(dir + filename)
+
+    return filenames
 
 
 def window():
@@ -19,4 +52,7 @@ def window():
 
 
 if __name__ == '__main__':
+    # get paths to images
+    img_paths = get_img_paths(input_folder, file_extensions)
+
     window()
