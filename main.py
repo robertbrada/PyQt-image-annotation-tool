f7af59b9b8504980862eae264242fbfdc4b2e12f
RobertBrada <bert.blader@gmail.com>
2019-11-30 10:15:16 +0100

    displaying label buttons and passing label as an argument of onClick event

--- main.py
+++ main.py
@@ -37,23 +37,47 @@ def get_img_paths(dir, extensions=''):
 
 
 class App(QWidget):
-    def __init__(self):
+    def __init__(self, labels, img_paths, output_file):
         super().__init__()
+
+        # init UI state
         self.title = 'PyQt5 - Annotation tool'
         self.left = 200
         self.top = 200
         self.width = 960
         self.height = 540
+
+        # init data for labels and images
+        self.counter = 0
+        self.img_paths = img_paths
+        self.labels = labels
+        self.num_labels = len(labels)
+        self.num_images = len(img_paths)
+
+        self.label_buttons = []
+
+        # Initialize image variables
+        self.image_raw = None
+        self.image = None
+
+        # init UI
         self.initUI()
+
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # Create button
-        button = QtWidgets.QPushButton(self)
-        button.setText("Hello World!")
-        button.move(50, 20)
+        # Create label button
+        for i, label in enumerate(self.labels):
+            self.label_buttons.append(QtWidgets.QPushButton(self))
+            button = self.label_buttons[i]
+            button.setText(label)
+            # 80 is button width, 10 is spacing between buttons
+            button.move((80 + 10) * i + 300, 20)
+
+            # https://stackoverflow.com/questions/35819538/using-lambda-expression-to-connect-slots-in-pyqt
+            button.clicked.connect(lambda state, x=label: self.set_label(x))
 
         # apply styles
         sshFile = "./styles/button.qss"
@@ -61,11 +85,15 @@ class App(QWidget):
             self.setStyleSheet(fh.read())
 
 
+    def set_label(self, label):
+        print(f"Label set as: {label}!")
+
+
 if __name__ == '__main__':
     # get paths to images
     img_paths = get_img_paths(input_folder, file_extensions)
 
     app = QApplication(sys.argv)
-    ex = App()
+    ex = App(labels, img_paths, output_file)
     ex.show()
     sys.exit(app.exec_())
