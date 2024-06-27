fa6a691ff0210274eb85f1592f977f2e070910e5
Jendrik Orthmann <jendrik.orthmann@student.jade-hs.de>
2024-06-27 18:12:27 +0000

    Fixed image names

--- test/test_generate_csv.py
+++ test/test_generate_csv.py
@@ -24,7 +24,7 @@ def test_generate_csv_german(app, qtbot, tmpdir):
         lines = f.readlines()
 
     assert lines[0].strip() == 'img;landschaft;menschen'
-    assert lines[1].strip() == 'test1;1;0'
+    assert lines[1].strip() == 'test1.jpg;1;0'
     assert lines[2].strip() == 'test2.BMP;1;0'
     assert lines[3].strip() == 'test3.jpg;0;1'
     assert lines[4].strip() == 'test4.gif;0;1'
@@ -33,7 +33,7 @@ def test_generate_csv_no_german_format(app, qtbot, tmpdir):
     # Set up the environment for the test
     app.german_format_check_box.setChecked(False)
     app.assigned_labels = {
-        'test1': ['landschaft'],
+        'test1.jpg': ['landschaft'],
         'test2.BMP': ['landschaft'],
         'test3.jpg': ['menschen'],
         'test4.gif': ['menschen']
@@ -45,7 +45,7 @@ def test_generate_csv_no_german_format(app, qtbot, tmpdir):
         lines = f.readlines()
 
     assert lines[0].strip() == 'img,landschaft,menschen'
-    assert lines[1].strip() == 'test1,1,0'
+    assert lines[1].strip() == 'test1.jpg,1,0'
     assert lines[2].strip() == 'test2.BMP,1,0'
     assert lines[3].strip() == 'test3.jpg,0,1'
     assert lines[4].strip() == 'test4.gif,0,1'
